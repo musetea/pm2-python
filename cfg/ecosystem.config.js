1af482c941b193a9f62ac3768e304dc333a5f224
muestea <dev0019@naver.com>
2022-04-20 12:25:55 +0900

    config

--- cfg/ecosystem.config.js
+++ cfg/ecosystem.config.js
@@ -21,16 +21,16 @@ module.exports = {
             watch:false,    
             stop_exit_codes: [0]        
         },
-        // {
-        //     name:"job310",
-        //     script:"src/main.py",
-        //     interpreter:"venv310/Scripts/python.exe",
-        //     instances:1,
-        //     exec_mode:"fork", //"cluster",
-        //     cron_restart:"*/1 * * * *",
-        //     autorestart: false,
-        //     watch:false,    
-        //     stop_exit_codes: [0]        
-        // }
+        {
+            name:"job310",
+            script:"src/main.py",
+            interpreter:"venv310/Scripts/python.exe",
+            instances:2,
+            exec_mode:"fork", //"cluster",
+            cron_restart:"*/1 * * * *",
+            autorestart: false,
+            watch:false,    
+            stop_exit_codes: [0]        
+        }
     ]
 }
